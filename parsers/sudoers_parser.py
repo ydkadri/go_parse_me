1ec8e4f33697179862ae1fc43dca996c19960515
Youcef Kadri <youcef.kadri@servian.com>
2021-12-21 20:37:36 +0000

    updated sudoers parser to take a delimiter input

--- parsers/sudoers_parser.py
+++ parsers/sudoers_parser.py
@@ -48,13 +48,13 @@ def _get_relevant_sudoers_lines(lines):
             yield line
 
 
-def _parse_sudoers_aliases(lines):
+def _parse_sudoers_aliases(lines, delim):
     aliases = []
     for line in lines:
         match = search(SUDO_ALIASES_PATTERN, line)
         if match is not None:
             alias = dict(zip(SUDO_ALIASES_HEADERS, match.groups()))
-            alias_detail = "|".join(
+            alias_detail = f"{delim}".join(
                 [detail.strip() for detail in alias["alias_detail"].split(",")]
             )
             alias["alias_detail"] = alias_detail
@@ -62,7 +62,7 @@ def _parse_sudoers_aliases(lines):
     return aliases
 
 
-def _parse_users_and_groups(lines):
+def _parse_users_and_groups(lines, delim):
     entities = {
         "users": [],
         "groups": [],
@@ -78,7 +78,7 @@ def _parse_users_and_groups(lines):
         users_match = search(SUDO_USERS_PATTERN, line)
         if users_match is not None:
             user = dict(zip(SUDO_USERS_HEADERS, users_match.groups()))
-            commands = "|".join(
+            commands = f"{delim}".join(
                 [command.strip() for command in user["commands"].split(",")]
             )
             user["commands"] = commands
@@ -88,7 +88,7 @@ def _parse_users_and_groups(lines):
         groups_match = search(SUDO_GROUPS_PATTERN, line)
         if groups_match is not None:
             group = dict(zip(SUDO_GROUPS_HEADERS, groups_match.groups()))
-            commands = "|".join(
+            commands = f"{delim}".join(
                 [command.strip() for command in group["commands"].split(",")]
             )
             group["commands"] = commands
@@ -100,10 +100,10 @@ def _parse_users_and_groups(lines):
     return entities
 
 
-def parse_sudoers(sudoers_file):
+def parse_sudoers(sudoers_file, delim="\n"):
     """A parser for /etc/passwd files taking an open file objet as input"""
     lines = sudoers_file.readlines()
     clean_lines = list(_get_relevant_sudoers_lines(lines))
-    aliases = _parse_sudoers_aliases(clean_lines)
-    entities = _parse_users_and_groups(clean_lines)
+    aliases = _parse_sudoers_aliases(clean_lines, delim)
+    entities = _parse_users_and_groups(clean_lines, delim)
     return aliases, entities
